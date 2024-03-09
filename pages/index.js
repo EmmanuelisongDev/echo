7e4b840fe28e6a8d7c12ff87d8456fc664c222ac
Emmanuel Isong <emmisongdev@gmail.com>
2024-03-09 23:34:06 +0100

    Added custom hook

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,12 @@
 import UsernameForm from "@/components/UsernameForm";
+import useUserInfo from "@/hooks/useUserInfo";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 
 export default function Home() {
   const { data: session, status } = useSession();
-  const [userInfo, setUserInfo] = useState();
-  const [userInfoStatus, setUserInfoStatus] = useState("false");
-  function getUserInfo() {
-    if (status === "loading") {
-      return;
-    }
-    fetch(`/api/users?id=${session.user.id}`).then((response) => {
-      response.json().then((json) => {
-        setUserInfo(json.user);
-        setUserInfoStatus("true");
-      });
-    });
-  }
 
-  useEffect(() => {
-    getUserInfo();
-  }, [status]);
+  const { userInfo, status: userInfoStatus } = useUserInfo;
   if (userInfoStatus === "loading") {
     return "Loading user information";
   }
